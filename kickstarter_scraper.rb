b631ccf6856a5e4ab27cafcd49b2f54c47288f1a
Heather Moreira <moreiraheather@gmail.com>
2020-04-11 22:55:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,3 +14,4 @@ create_project_hash
 # project_title: project.css("h2.bbcard_name strong a").text
 # image ling: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
